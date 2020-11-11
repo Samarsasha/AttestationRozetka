144083c70e69b86d02d84b4d3d0dae35f5dae34b
samar <samarsasha@gmail.com>
2020-11-11 20:46:27 +0200

    test

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.inputmethodservice.Keyboard;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextCity;
+    private TextView textViewWeather;
+    private final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&appid=a8dc76af8cdf4cf1ff9d20bbf3f89fb3&lang=ru&units=metric";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextCity = findViewById(R.id.editTextCity);
+        textViewWeather = findViewById(R.id.textViewWeather);
+
+    }
+
+    public void onClickShowWeather(View view) {
+        String city = editTextCity.getText().toString().trim();
+        if (!city.isEmpty()) {
+            DownloadWeatherTask task  = new DownloadWeatherTask();
+            String url = String.format(WEATHER_URL, city);
+            task.execute(url);
+            hideSoftKeyboard(this);
+            editTextCity.setText("");
+        }
+    }
+
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    private class DownloadWeatherTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null) {
+                    result.append(line);
+                    line = reader.readLine();
+                }
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String city = jsonObject.getString("name");
+                String temp = jsonObject.getJSONObject("main").getString("temp");
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String pressure = jsonObject.getJSONObject("main").getString("pressure");
+                String weather = String.format("%s\nТемпература: %s°C\nНа улице: %s\nАтмосферное давление: %s mm Hg\n \"Розетка\" желает Вам удачного дня", city, temp, description, pressure);
+                textViewWeather.setText(weather);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
